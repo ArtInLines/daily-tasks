0bff92df48899ecd503ea13bf38c6cec25ceb00a
ArtInLines <linesinart@gmail.com>
2020-11-26 19:03:31 +0100

    Test-Logs minified to only necessary info

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
+/**
+ *
+ * @param {Array} arr A 2D Array containing the input and expected output for each example, where `arr[i][0] == example input` and `arr[i][1] == expected output`
+ * @param {Function} func The Method taking the input of `arr` as argument
+ */
 function test(arr, func) {
+	let err = false;
 	arr.map((el) => {
-		console.log(el[0], '-', 'Expected:', el[1], 'Output:', func(el[0]));
+		if (el[1] !== func(el[0])) {
+			console.log(`${el[0]} - Expected: ${el[1]} - Output: ${func(el[0])}`);
+			err = true;
+		}
 	});
+	if (!err) console.log(`Testing ${func.name} finished without errors \\o/`);
 }
 
 module.exports = { test };
