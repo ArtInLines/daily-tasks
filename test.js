f06b3a87277fa9f6859a52f6a388ad525c8a7aa7
ArtInLines <linesinart@gmail.com>
2020-11-29 21:24:57 +0100

    Array return values support

--- test.js
+++ test.js
@@ -5,12 +5,18 @@
  */
 function test(arr, func) {
 	let err = false;
-	arr.map((el) => {
-		if (el[1] !== func(el[0])) {
-			console.log(`${el[0]} - Expected: ${el[1]} - Output: ${func(el[0])}`);
-			err = true;
-		}
-	});
+	for (let i = 0; i < arr.length; i++) {
+		const el = arr[i],
+			expected = el[1],
+			input = el[0],
+			output = func.apply(undefined, input);
+		if (Array.isArray(expected) && Array.isArray(output)) {
+			for (let j = 0; j < expected.length; j++) {
+				if (expected[j] !== output[j]) err = true;
+			}
+		} else if (expected != output) err = true;
+		if (err) console.log(`${input} - Expected: ${expected} - Output: ${output}`);
+	}
 	if (!err) console.log(`Testing ${func.name} finished without errors \\o/`);
 }
 
