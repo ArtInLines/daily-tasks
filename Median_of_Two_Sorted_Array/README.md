7b9d25694eb98b2e3af6ca803ebd59fc1cf507a9
ArtInLines <linesinart@gmail.com>
2021-02-02 23:08:49 +0100

    Median of Two Sorted Arrays

---
+++ Median_of_Two_Sorted_Array/README.md
@@ -0,0 +1,15 @@
+# Median of Two Sorted Array
+
+Task from [leetcode](https://leetcode.com/problems/median-of-two-sorted-arrays)
+
+## Prompt:
+
+Given two sorted arrays `nums1` and `nums2` of size `m` and `n` respectively, return the **median** of the two sorted arrays.
+
+## Solution:
+
+Merging both arrys into a sorted array and then getting the median. (Far from the best solution)
+
+## Imporvement:
+
+Use two pointers to go through the two arrays, to find the solution in a time complexity of `O(log(m+n))`
---
+++ Median_of_Two_Sorted_Array/index.js
@@ -0,0 +1,69 @@
+/**
+ * Finds the median of two sorted Arrays of numbers
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+function findMedianSortedArrays(nums1, nums2) {
+	nums1 = merge(nums1, nums2);
+	if (nums1.length % 2 === 0) {
+		const half = nums1.length / 2;
+		return (nums1[half] + nums1[half - 1]) / 2;
+	} else return nums1[(nums1.length - 1) / 2];
+}
+
+/**
+ * Merges to sorted Arrays into a single sorted array.
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+function merge(nums1, nums2) {
+	if (nums1.length === 0) return nums2;
+	else if (nums2.length === 0) return nums1;
+
+	let i = 0;
+	while (nums1.length) {
+		// Doesn't need to check nums2.length, cause nums2 will be returned anyways
+		if (nums1[0] <= nums2[i]) nums2.splice(i, 0, nums1.shift());
+		i++;
+		if (i >= nums2.length) {
+			nums2 = nums2.concat(nums1);
+			break;
+		}
+	}
+	return nums2;
+}
+
+//
+//
+// Examples
+//
+
+const examples = [
+	[
+		[
+			[1, 3, 6],
+			[4, 5, 8, 15],
+		],
+		5,
+	],
+	[[[0], [1, 4, 7, 337]], 4],
+	[
+		[
+			[5, 7, 9, 11, 13, 15],
+			[0, 0, 2, 2, 2],
+		],
+		5,
+	],
+	[[[2], [0]], 1],
+	[
+		[
+			[0, 0],
+			[0, 0, 0, 100],
+		],
+		0,
+	],
+];
+const { test } = require('./../test');
+test(examples, findMedianSortedArrays);
