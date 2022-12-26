cc9acea4ed0ed535b49c313412cc935fa5d7c5d9
ArtInLines <linesinart@gmail.com>
2022-12-26 22:39:36 +0100

    Updated allTests

--- alltests.js
+++ alltests.js
@@ -1,32 +1,40 @@
 const process = require('process');
 const { spawn } = require('child_process');
+const { readFileSync, readdirSync } = require('fs');
 
-const testScripts = [
-	['py', './test.py'],
-	['node', './test.js'],
-];
+const languages = JSON.parse(readFileSync('./languages.json', { encoding: 'utf8' }));
 const argv = process.argv.slice(2);
-runScript(0, testScripts);
+const filesInDir = readdirSync(argv[0], { withFileTypes: true })
+	.filter((d) => d.isFile())
+	.map((d) => d.name);
+runTests();
 
-async function runScript(idx = 0, scripts = testScripts) {
-	if (idx >= scripts.length) return;
+async function runTests(idx = 0) {
+	if (idx >= languages.length) return;
 
-	let script = scripts[idx];
-	const s = spawn(script[0], [script[1], ...argv]);
-	console.log(`\nRunning ${script[0]} script...`);
-	s.stdout.on('data', (chunk) => {
-		console.log(String(chunk));
-	});
-	s.stderr.on('data', (chunk) => {
-		console.log(String(chunk));
-	});
-	return new Promise((resolve, reject) => {
-		s.on('close', () => {
-			runScript(idx + 1, scripts).then(resolve);
+	let lang = languages[idx];
+	if (!filesInDir.some((f) => lang['endings'].some((ending) => new RegExp('.' + ending + '$', 'i').test(f)))) {
+		return runTests(idx + 1);
+	} else {
+		const s = spawn(lang['cmd'], [lang['test'], ...argv]);
+		console.log(`\nRunning test in ${lang['name']}...`);
+
+		s.stdout.on('data', (chunk) => {
+			console.log(String(chunk));
 		});
 
-		s.on('error', (err) => {
-			reject(err);
+		s.stderr.on('data', (chunk) => {
+			console.log(String(chunk));
 		});
-	});
+
+		return new Promise((resolve, reject) => {
+			s.on('close', () => {
+				runTests(idx + 1).then(resolve);
+			});
+
+			s.on('error', (err) => {
+				reject(err);
+			});
+		});
+	}
 }
