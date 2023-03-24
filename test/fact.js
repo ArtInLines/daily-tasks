3b1ae092cc17b4ab690b7ee135976be03e22da52
ArtInLines <linesinart@gmail.com>
2023-03-24 18:54:15 +0100

    Removed tests for testing framework

--- test/fact.js
+++
@@ -1,7 +0,0 @@
-// Factorial
-function fact(n) {
-	if (n <= 1) return 1;
-	return n * fact(n - 1);
-}
-
-console.log(fact(Number(require('process').argv[2])));
--- test/fib.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-def fib(n):
-	table = [0, 1]
-	for _ in range(len(table), n+1):
-		table.append(table[-2] + table[-1])
-	return table[-1]
-
-print(fib(int(argv[1])))
